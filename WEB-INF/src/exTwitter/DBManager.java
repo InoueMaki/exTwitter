7ecc86bb49813ed1d0f7814597d036c31448578f
takahiro4432 <t-sato@excite-software.co.jp>
2015-08-03 15:46:17 +0900

    DBManagerの修正
    exeUpdate,getResultSet両メソッドをそれぞれSQLExceptionをthrowするように変更した。
    createTable,DropTable両メソッドを削除した

--- WEB-INF/src/exTwitter/DBManager.java
+++ WEB-INF/src/exTwitter/DBManager.java
@@ -14,18 +14,17 @@ import java.util.Properties;
 /**
  * <b>DBManager</b><br/>
  * public class DBManager<br/>
- * <blockquote>
- * SQLiteとのデータのやり取りを行う
- * </blockquote>
+ * <blockquote> SQLiteとのデータのやり取りを行う </blockquote>
+ * 
  * @author sato
  */
 public class DBManager {
 	private Connection con = null;
 	private Statement smt = null;
 	private ResultSet rs = null;
-	private String  db_base = "";
-	
-	static{		
+	private String db_base = "";
+
+	static {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			System.out.println("Driver Load");
@@ -34,14 +33,12 @@ public class DBManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * <b>DBManager</b><br/>
 	 * public DBManager()<br/>
-	 * <blockquote>
-	 * コンストラクタ<br>
-	 * プロパティファイルのロードを行う
-	 * </blockquote>
+	 * <blockquote> コンストラクタ<br>
+	 * プロパティファイルのロードを行う </blockquote>
 	 */
 	public DBManager() {
 		final Properties config = new Properties();
@@ -53,7 +50,7 @@ public class DBManager {
 			config.load(inputStream);
 			// プロパティファイル内の変数読み出し
 			db_base = config.getProperty("db_base");
-			System.out.println("\tproterty: db_base='"+db_base+"'");
+			System.out.println("\tproterty: db_base='" + db_base + "'");
 		} catch (IOException e) {
 			System.err.println("Property Load Error\n");
 			e.printStackTrace();
@@ -63,17 +60,17 @@ public class DBManager {
 	/**
 	 * <b>getConnection</b><br/>
 	 * public void getConnection() throws SQLException<br/>
-	 * <blockquote>
-	 * コネクション生成<br>
+	 * <blockquote> コネクション生成<br>
 	 * DB: sample にアクセスを行う<br>
-	 * 最大3回までトライする
-	 * </blockquote>
+	 * 最大3回までトライする </blockquote>
+	 * 
 	 * @throws SQLException
 	 */
-	public void getConnection() throws SQLException{
+	public void getConnection() throws SQLException {
+		System.out.println("DB url: " + db_base + "sample");
 		for (int i = 0; i < 3; i++) {
 			try {
-				this.con = DriverManager.getConnection(this.db_base+"sample");
+				this.con = DriverManager.getConnection(this.db_base + "sample");
 				this.smt = con.createStatement();
 				System.out.println("Create Connection");
 				break;
@@ -92,19 +89,18 @@ public class DBManager {
 	/**
 	 * <b>getConnection</b><br/>
 	 * public void getConnection(String DB_name) throws SQLException<br/>
-	 * <blockquote>
-	 * コネクション生成<br>
+	 * <blockquote> コネクション生成<br>
 	 * DB名を指定しアクセスを行う<br>
-	 * 最大3回までトライする
-	 * </blockquote>
-	 * @param データベース名
+	 * 最大3回までトライする </blockquote>
+	 * 
+	 * @param DB_name
 	 * @throws SQLException
 	 */
 	public void getConnection(String DB_name) throws SQLException {
+		System.out.println("DB url: " + db_base + DB_name);
 		for (int i = 0; i < 3; i++) {
 			try {
-				this.con = DriverManager
-						.getConnection(this.db_base + DB_name);
+				this.con = DriverManager.getConnection(this.db_base + DB_name);
 				this.smt = con.createStatement();
 				System.out.println("Create Connection");
 				break;
@@ -122,95 +118,47 @@ public class DBManager {
 	}
 
 	/**
-	 * <b>createTable</b><br/>
-	 * public void createTable(String sql) throws SQLException<br/>
-	 * <blockquote>
-	 * テーブル名の生成を行う<br>
-	 * すでに存在する場合例外を返す
+	 * <b>exeUpdate</b><br/>
+	 * public int exeUpdate(String sql) throws SQLException<br/>
+	 * <blockquote> 要素の更新・削除を行う<br>
 	 * </blockquote>
-	 * @param SQLクエリ
+	 * 
+	 * @param sql
+	 * @return int <該当行数>
 	 * @throws SQLException
 	 */
-	public void createTable(String sql) throws SQLException {
-		System.out.print("\ttry '" + sql + "'");
-		smt.execute(sql);
-		System.out.println("...O.K.");
-	}
-
-	/**
-	 * <b>dropTable</b><br/>
-	 * public void dropTable(String sql)<br/>
-	 * <blockquote>
-	 * テーブルの削除を行う<br>
-	 * 削除に失敗した場合スタックトレースを出力する
-	 * </blockquote>
-	 * @param SQLクエリ
-	 */
-	public void dropTable(String sql) {
-		System.out.print("\ttry '" + sql + "'\t");
-		try {
-			smt.execute(sql);
-			System.out.println("... O.K.");
-		} catch (SQLException e) {
-			System.err.println("\nDrop Error\tqry:'" + sql + "'\n");
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * <b>exeUpdate</b><br/>
-	 * public int exeUpdate(String sql)<br/>
-	 * <blockquote>
-	 * 要素の更新・削除を行う<br>
-	 * 更新・削除に失敗した場合スタックトレースを出力する
-	 * </blockquote>
-	 * @param SQLクエリ
-	 * @return int <該当行数>
-	 */
-	public int exeUpdate(String sql) {
+	public int exeUpdate(String sql) throws SQLException {
 		int count = 0;
-		System.out.print("\ttry '" + sql + "'\t");
-		try {
-			// データの挿入
-			count = smt.executeUpdate(sql);
-			System.out.println("... O.K.");
-		} catch (SQLException e) {
-			System.err.println("\nUpdate Error\tqry:'" + sql + "'\n");
-			e.printStackTrace();
-		}
+		System.out.println("\ttry '" + sql + "'\t");
+		// データの挿入
+		count = smt.executeUpdate(sql);
+		System.out.println("... O.K.");
 		return count;
 	}
 
 	/**
 	 * <b>getResultSet</b><br/>
-	 * public ResultSet getResultSet(String sql)<br/>
-	 * <blockquote>
-	 * 要素の取得を行う<br>
-	 * 取得に失敗した場合スタックトレースを出力する
+	 * public ResultSet getResultSet(String sql) throws SQLException<br/>
+	 * <blockquote> 要素の取得を行う<br>
 	 * </blockquote>
-	 * @param SQLクエリ
+	 * 
+	 * @param sql
 	 * @return ResultSet
+	 * @throws SQLException
 	 */
-	public ResultSet getResultSet(String sql) {
+	public ResultSet getResultSet(String sql) throws SQLException {
 		System.out.print("\ttry '" + sql + "'\t");
-		try {
-			this.rs = smt.executeQuery(sql);
-			System.out.println("... O.K.");
-		} catch (SQLException e) {
-			System.err.println("\nSelect Error\tqry:'" + sql + "'\n");
-			e.printStackTrace();
-		}
+		this.rs = smt.executeQuery(sql);
+		System.out.println("... O.K.");
 		return this.rs;
 	}
 
 	/**
 	 * <b>closeConnection</b><br/>
 	 * public void closeConnection()<br/>
-	 * <blockquote>
-	 * コネクション・ステートメントの切断を行う<br>
+	 * <blockquote> コネクション・ステートメントの切断を行う<br>
 	 * リザルトセットが生成されていた場合切断を行う<br>
-	 * コネクション・ステートメント切断に失敗した場合スタックトレースを出力する
-	 * </blockquote>
+	 * コネクション・ステートメント切断に失敗した場合スタックトレースを出力する </blockquote>
 	 */
 	public void closeConnection() {
 		System.out.println("Closing...");
@@ -223,7 +171,7 @@ public class DBManager {
 				// Select文が一度も実行されていない
 			}
 		}
-		
+
 		// StatementとConnectionの切断
 		try {
 			this.smt.close();
