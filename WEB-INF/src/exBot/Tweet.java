8b2209bd3dc45d513b08ab0aebcf552817bed2dd
takahiro4432 <t-sato@excite-software.co.jp>
2015-08-04 18:13:44 +0900

    BOT部修正
    SQLite仕様にSQL文を修正しました
    DBManager変更に伴い修正しました
    twitter4jプロパティファイルを置いて実行してください

--- WEB-INF/src/exBot/Tweet.java
+++ WEB-INF/src/exBot/Tweet.java
@@ -3,31 +3,36 @@ package exBot;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
 
-
+/**
+ * twitterAPIにアクセスし処理を行う
+ * @author sato
+ */
 public class Tweet {
+	private Twitter twitter;
+	/**
+	 * <b>Tweet</b><br/>
+	 * public Tweet()<br/>
+	 * <blockquote> コンストラクタ<br>
+	 * プロパティファイルのロードを行う </blockquote>
+	 */
+	public Tweet() {
+		twitter = new TwitterFactory().getInstance();
+	}
 	
-	//Twitterアカウント情報
-	//customerKey
-	//accessToken
-
+	/**
+	 * <b>sendTweet</b><br/>
+	 * public void sendTweet(String text)<br/>
+	 * テキストをツイッターに投稿する
+	 * @param text
+	 */
 	public void sendTweet(String text){
-		
-		Twitter twitter = new TwitterFactory().getInstance();
-		twitter.setOAuthConsumer(consumerKey, consumerSecret);
-		twitter.setOAuthAccessToken(new AccessToken(accessToken,accessTokenSecret));
-		
 		try{
-			
 			twitter.updateStatus(text);
 			System.out.println("ツイート成功");
-			
 		} catch(TwitterException e){
-			
 			System.err.println("ツイート失敗："+text);
-			System.err.println(e.getMessage());
+			e.printStackTrace();
 		}
-		
 	}
 }
