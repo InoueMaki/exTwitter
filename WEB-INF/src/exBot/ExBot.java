6f44ecbf9f7713cbf78dadbcab7ed9dfa82dd147
takahiro4432 <t-sato@excite-software.co.jp>
2015-08-05 18:13:09 +0900

    BOT修正
    ループ内で生成されていたインスタンスを
    外部にだし使いまわしするようにしました

--- WEB-INF/src/exBot/ExBot.java
+++ WEB-INF/src/exBot/ExBot.java
@@ -11,46 +11,56 @@ import exTwitter.DBManager;
 
 /**
  * DBから投稿条件に該当するツイートを収集しツイートする
+ * 
  * @author excite
- *
+ * 
  */
 public class ExBot {
 
-	// main関数
+	/**
+	 * <b>main</b><br/>
+	 * public static void main(String[] args)<br/>
+	 * <blockquote> BOT本体<br>
+	 * 5分おきに検索条件に該当するツイートをDBから収集し TwitterAPIに投稿する </blockquote>
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
 		// ツイート本文格納
 		ArrayList<String> tweets = new ArrayList<String>();
-
-		// 予定時刻
+		// 投稿予定時刻
 		long search = System.currentTimeMillis();
-
 		// 時間フォーマット指定
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-		// 開始時刻表示
+		// プログラム稼働時刻表示
 		System.out.println("startTime = " + sdf.format(search));
+
+		// ツイッターAPI接続
+		Tweet tweet = new Tweet();
 
 		// BOT無限ループ
 		while (true) {
-			// 現在時刻よりも予定時間が過去にある
+			
+			// 条件：現在時刻よりも投稿予定時間が過去にある
 			if (search < System.currentTimeMillis()) {
-				System.out.println(sdf.format(search));
+				// 検索開始をコンソール表示
+				System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+				// 投稿予定時刻表示
+				System.out.println("searchTime = "+sdf.format(search));
+				// ツイート本文格納クリア
 				tweets.clear();
 
-				// 次回検索時間設定
+				// 次回検索時間を5分後に設定
 				search += 1000 * 60 * 5;
 
 				// 定期ツイート収集
 				tweets.addAll(getRoutineTweet());
-
 				// 単発ツイート収集・投稿済みフラグON
 				tweets.addAll(getOnceTweet());
 
-				// ツイート投稿
-				Tweet tweet =new Tweet();
-				int i;
-				for (i = 0; i < tweets.size(); i++) {
+				// 収集ツイート全件投稿
+				for (int i = 0; i < tweets.size(); i++) {
 					System.out.println("tweets\t" + tweets.get(i));
 					tweet.sendTweet(tweets.get(i));
 				}
@@ -63,7 +73,7 @@ public class ExBot {
 	private static ArrayList<String> getRoutineTweet() {
 		ArrayList<String> tweets = new ArrayList<String>();
 
-		// 今日の日時
+		// 現在のの日時(sql.Date)
 		java.sql.Date date = new java.sql.Date(System.currentTimeMillis());
 
 		Calendar cal = Calendar.getInstance();
@@ -84,62 +94,55 @@ public class ExBot {
 		String time = "post_time >= '" + start_time + "' and post_time <'"
 				+ end_time + "'";
 
+		// 日付指定クエリ
 		String qry1 = "SELECT text FROM " + monthly + "WHERE (" + span
 				+ " and day=" + day + " and " + time + ");";
-		
-		// 月末用クエリ（day=0）
+		// 日付指定月末用クエリ（day=0）
 		String qry2 = "SELECT text FROM " + monthly + "WHERE (" + span
-		+ " and day=0 and " + time + ");";
-		
+				+ " and day=0 and " + time + ");";
+		// 曜日指定クエリ
 		String qry3 = "SELECT text FROM " + weekly + "WHERE (" + span
 				+ " and day=" + week + " and " + time + ");";
 
 		// コネクション確立
 		DBManager DBM = new DBManager();
 		try {
-			DBM.getConnection("excite");
-		} catch (SQLException e2) {
-			e2.printStackTrace();
+			DBM.getConnection("excite.db");
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
 		// monthly
-		
 		try {
-			ResultSet rs1 = DBM.getResultSet(qry1);
+			ResultSet rs1;
+			if (!isLastDay()) {
+				// 通常monthly
+				rs1 = DBM.getResultSet(qry1);
+			} else {
+				// 月末monthly
+				rs1 = DBM.getResultSet(qry2);
+			}
 			while (rs1.next()) {
 				tweets.add(rs1.getString("text"));
 			}
 		} catch (SQLException e) {
 			System.err.println("定期ツイート検索できません");
+			e.printStackTrace();
 		}
-		
-		//monthly月末
-		if(isLastDay()){
-			
-			try {
-				ResultSet rs2 = DBM.getResultSet(qry2);
-				while (rs2.next()) {
-					tweets.add(rs2.getString("text"));
-				}
-			} catch (SQLException e) {
-				System.err.println("定期ツイート検索できません");
-			}
-		}
-		
+
 		// weekly
-		
 		try {
-			ResultSet rs3 = DBM.getResultSet(qry3);
-			while (rs3.next()) {
-				tweets.add(rs3.getString("text"));
+			ResultSet rs2 = DBM.getResultSet(qry3);
+			while (rs2.next()) {
+				tweets.add(rs2.getString("text"));
 			}
 		} catch (SQLException e) {
 			System.err.println("定期ツイート検索できません");
+			e.printStackTrace();
 		}
 
 		// コネクション切断
 		DBM.closeConnection();
-
 		return tweets;
 	}
 
@@ -147,48 +150,45 @@ public class ExBot {
 	private static ArrayList<String> getOnceTweet() {
 		ArrayList<String> tweets = new ArrayList<String>();
 		ArrayList<Integer> ids = new ArrayList<Integer>();
-		
+
 		// 時間フォーマット指定
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		
+
 		// 予定時刻
 		String now = sdf.format(System.currentTimeMillis());
 
-		
-		
 		// クエリ作成
 		String qry1 = "SELECT text,once_id from once where (posted =0 and reserve_time < '"
 				+ now + "');";
 		// コネクション確立
 		DBManager DBM = new DBManager();
 		try {
-			DBM.getConnection("excite");
-		} catch (SQLException e1) {
-			
-			e1.printStackTrace();
+			DBM.getConnection("excite.db");
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
-		int c = 0;
+
 		try {
 			ResultSet rs1 = DBM.getResultSet(qry1);
 			while (rs1.next()) {
-				c++;
 				tweets.add(rs1.getString("text"));
 				ids.add(rs1.getInt("once_id"));
 			}
 		} catch (SQLException e) {
 			System.err.println("単発ツイート検索できません");
+			e.printStackTrace();
 		}
-		System.out.println(c);
 
-		// 投稿完了に更新
-		String conf = "";
+		// 収集したツイートを投稿完了に更新
+		// 更新用条件文の作成
+
 		int i;
-		// IDから条件を作成
+		String conf = "";
+		// idsから条件を作成(全体-1件まで)
 		for (i = 0; i < ids.size() - 1; i++) {
 			conf += "once_id=" + ids.get(i) + " or ";
 		}
-		// IDが取得されない場合偽
+		// 1件以上のidが取得される(条件文'or'のため最後の1件のみこちらで対応)
 		if (i < ids.size()) {
 			conf += "once_id=" + ids.get(i);
 
@@ -204,23 +204,30 @@ public class ExBot {
 			}
 
 			System.out.println(count + "件を投稿済みにしました");
-		}System.out.println(conf);
+		}
 		// コネクション切断
 		DBM.closeConnection();
 
 		return tweets;
 	}
-	
-	
-	private static boolean isLastDay(){
+
+	/**
+	 * <b>isLastDay</b><br>
+	 * private static boolean isLastDay()<br/>
+	 * <blockquote> 今日が月末かどうかを返す </blockquote>
+	 * 
+	 * @return 月末:true その他:false
+	 */
+	private static boolean isLastDay() {
 		// Calendar型インスタンス
 		Calendar cal = Calendar.getInstance();
 		// 月末
-	    int last =cal.getActualMaximum(Calendar.DATE);
-	    // 今日
-	    int day  = cal.get(Calendar.DATE);
-	    // 一致するときtrueを返す
-	    if(last==day)return true;
+		int last = cal.getActualMaximum(Calendar.DATE);
+		// 今日
+		int day = cal.get(Calendar.DATE);
+		// 一致するときtrueを返す
+		if (last == day)
+			return true;
 		return false;
 	}
 }
