279779463815c9165fb71a7585154b4a871cddde
takahiro4432 <t-sato@excite-software.co.jp>
2015-08-12 11:07:53 +0900

    メソッドの分割
    大きくなりすぎていたメソッドを一部分割しました

--- WEB-INF/src/exTwitter/Scheduler.java
+++ WEB-INF/src/exTwitter/Scheduler.java
@@ -50,26 +50,18 @@ public class Scheduler {
 		session.setAttribute("sche", "on");
 		clearSchedule(session);
 		Calendar calendar = Calendar.getInstance();
-		Object objYear = session.getAttribute("year");
-		Object objMonth = session.getAttribute("month");
-		if ((objYear != null) && (objMonth != null)) {
-			year = new Integer(objYear.toString()).intValue();
-			month = new Integer(objMonth.toString()).intValue();
-		} else {
-			if (year == 0) {
-				year = calendar.get(Calendar.YEAR);
-			}
-			if (month == 0) {
-				month = calendar.get(Calendar.MONTH);
-			}
+
+		if (year == 0 || month == 0) {
+			year = calendar.get(Calendar.YEAR);
+			month = calendar.get(Calendar.MONTH);
 		}
+
 		// 1日をセット
 		calendar.set(year, month, 1);
 		int startWeek = calendar.get(Calendar.DAY_OF_WEEK);
 		// 先月末日を取得
 		calendar.set(year, month, 0);
 		int beforeMonthlastDay = calendar.get(Calendar.DATE);
-
 		// 今月末日を取得
 		calendar.set(year, month + 1, 0);
 		int thisMonthlastDay = calendar.get(Calendar.DATE);
@@ -82,80 +74,40 @@ public class Scheduler {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
+		final String once_format = "SELECT * FROM once WHERE reserve_time>='%04d-%02d-%02d 00:00:00' and reserve_time<='%04d-%02d-%02d 23:59:59' and posted<2";
+		final String routine_format = "SELECT * FROM routine WHERE start_date<='%04d-%02d-%02d' and end_date>='%04d-%02d-%02d' and routine_id=%d and posted=0";
+		final String weekly_format = "SELECT routine_id FROM  weekly WHERE day=%d";
+		final String monthly_format = "SELECT routine_id FROM  monthly WHERE day=%d";
+
 		String sql;
 		String presql1;
 		String presql2;
-		ResultSet rs;
+		ResultSet rs = null;
 
 		// 先月分の日付を格納
 		for (int i = startWeek - 2; i >= 0; i--) {
-			sql = String
-					.format(
-							"SELECT * FROM once WHERE reserve_time>='%04d-%02d-%02d 00:00:00' and reserve_time<='%04d-%02d-%02d 23:59:59' and posted<2",
-							year, month, beforeMonthlastDay - i, year, month,
-							beforeMonthlastDay - i);
-			try {
-				rs = dbm.getResultSet(sql);
-				ArrayList<OnceBean> once = new ArrayList<OnceBean>();
-				while (rs.next()) {
-					OnceBean bean = new OnceBean();
-					bean.setOnceId(rs.getInt("once_id"));
-					bean.setText(rs.getString("text"));
-					bean.setReserveTime(rs.getString("reserve_time"));
-					bean.setPosted(rs.getInt("posted"));
-					once.add(bean);
-				}
-				rs.close();
-				OnceBeans.add(once);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			int week = (count + 1) % 7;
-			if (week == 0) {
-				week = 7;
-			}
-			presql1 = String.format(
-					"SELECT routine_id FROM  weekly WHERE day=%d", week);
+			sql = String.format(once_format, year, month,
+					(beforeMonthlastDay - i), year, month,
+					(beforeMonthlastDay - i));
+			rs = setOnceBeans(dbm, sql, rs);
+			int week = setWeek(count);
+			presql1 = String.format(weekly_format, week);
 			if (i == 0) {
-				presql2 = String.format(
-						"SELECT routine_id FROM  monthly WHERE day=%d", 0);
+				presql2 = String.format(monthly_format, i);
 			} else {
-				presql2 = String.format(
-						"SELECT routine_id FROM  monthly WHERE day=%d",
-						beforeMonthlastDay - i);
+				presql2 = String.format(monthly_format, beforeMonthlastDay - i);
 			}
 			ArrayList<Integer> ids = new ArrayList<Integer>();
 			try {
 				ArrayList<RoutineBean> routine = new ArrayList<RoutineBean>();
-				rs = dbm.getResultSet(presql1);
-				while (rs.next()) {
-					ids.add(rs.getInt("routine_id"));
-				}
-				rs.close();
-				rs = dbm.getResultSet(presql2);
-				while (rs.next()) {
-					ids.add(rs.getInt("routine_id"));
-				}
-				rs.close();
+				rs = addRoutineCand(dbm, presql1, ids);
+				rs = addRoutineCand(dbm, presql2, ids);
 				for (int j = 0; j < ids.size(); j++) {
-					sql = String
-							.format(
-									"SELECT * FROM routine WHERE start_date<='%04d-%02d-%02d' and end_date>='%04d-%02d-%02d' and routine_id=%d and posted=0",
-									year, month, beforeMonthlastDay - i, year,
-									month, beforeMonthlastDay - i, ids.get(j));
-					rs = dbm.getResultSet(sql);
-
-					while (rs.next()) {
-						RoutineBean bean = new RoutineBean();
-						bean.setRoutineId(rs.getInt("routine_id"));
-						bean.setTitle(rs.getString("title"));
-						bean.setText(rs.getString("text"));
-						bean.setStartDate(rs.getString("start_date"));
-						bean.setEndDate(rs.getString("end_date"));
-						bean.setPosted(rs.getInt("posted"));
-						routine.add(bean);
-					}
-					rs.close();
+					sql = String.format(routine_format, year, month,
+							(beforeMonthlastDay - i), year, month,
+							(beforeMonthlastDay - i), ids.get(j));
+					rs = addRoutine(dbm, sql, routine);
 
 				}
 				RoutineBeans.add(routine);
@@ -164,74 +116,29 @@ public class Scheduler {
 			}
 			calendarDay[count++] = beforeMonthlastDay - i;
 		}
-
+		
 		// 今月分の日付を格納
 		for (int i = 1; i <= thisMonthlastDay; i++) {
-			sql = String
-					.format(
-							"SELECT * FROM once WHERE reserve_time>='%04d-%02d-%02d 00:00:00' and reserve_time<='%04d-%02d-%02d 23:59:59' and posted<2",
-							year, month + 1, i, year, month + 1, i);
-			try {
-				rs = dbm.getResultSet(sql);
-				ArrayList<OnceBean> once = new ArrayList<OnceBean>();
-				while (rs.next()) {
-					OnceBean bean = new OnceBean();
-					bean.setOnceId(rs.getInt("once_id"));
-					bean.setText(rs.getString("text"));
-					bean.setReserveTime(rs.getString("reserve_time"));
-					bean.setPosted(rs.getInt("posted"));
-					once.add(bean);
-				}
-				rs.close();
-				OnceBeans.add(once);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			sql = String.format(once_format, year, month + 1, i, year,
+					month + 1, i);
+			rs = setOnceBeans(dbm, sql, rs);
 
-			int week = (count + 1) % 7;
-			if (week == 0) {
-				week = 7;
-			}
-			presql1 = String.format(
-					"SELECT routine_id FROM  weekly WHERE day=%d", week);
+			int week = setWeek(count);
+			presql1 = String.format(weekly_format, week);
 			if (i == thisMonthlastDay) {
-				presql2 = String.format(
-						"SELECT routine_id FROM  monthly WHERE day=%d", 0);
+				presql2 = String.format(monthly_format, 0);
 			} else {
-				presql2 = String.format(
-						"SELECT routine_id FROM  monthly WHERE day=%d", i);
+				presql2 = String.format(monthly_format, i);
 			}
 			ArrayList<Integer> ids = new ArrayList<Integer>();
 			try {
 				ArrayList<RoutineBean> routine = new ArrayList<RoutineBean>();
-				rs = dbm.getResultSet(presql1);
-				while (rs.next()) {
-					ids.add(rs.getInt("routine_id"));
-				}
-				rs.close();
-				rs = dbm.getResultSet(presql2);
-				while (rs.next()) {
-					ids.add(rs.getInt("routine_id"));
-				}
-				rs.close();
+				rs = addRoutineCand(dbm, presql1, ids);
+				rs = addRoutineCand(dbm, presql2, ids);
 				for (int j = 0; j < ids.size(); j++) {
-					sql = String
-							.format(
-									"SELECT * FROM routine WHERE start_date<='%04d-%02d-%02d' and end_date>='%04d-%02d-%02d' and routine_id=%d and posted=0",
-									year, month + 1, i, year, month + 1, i, ids
-											.get(j));
-					rs = dbm.getResultSet(sql);
-					while (rs.next()) {
-						RoutineBean bean = new RoutineBean();
-						bean.setRoutineId(rs.getInt("routine_id"));
-						bean.setTitle(rs.getString("title").trim());
-						bean.setText(rs.getString("text").trim());
-						bean.setStartDate(rs.getString("start_date").trim());
-						bean.setEndDate(rs.getString("end_date").trim());
-						bean.setPosted(rs.getInt("posted"));
-						routine.add(bean);
-					}
-					rs.close();
+					sql = String.format(routine_format, year, month + 1, i,
+							year, month + 1, i, ids.get(j));
+					rs = addRoutine(dbm, sql, routine);
 
 				}
 				RoutineBeans.add(routine);
@@ -245,69 +152,24 @@ public class Scheduler {
 		// 翌月分の日付を格納
 		int nextMonthDay = 1;
 		while (count % 7 != 0) {
-			sql = String
-					.format(
-							"SELECT * FROM once WHERE reserve_time>='%04d-%02d-%02d 00:00:00' and reserve_time<='%04d-%02d-%02d 23:59:59' and posted<2",
-							year, month + 2, nextMonthDay, year, month + 2,
-							nextMonthDay);
-			try {
-				rs = dbm.getResultSet(sql);
-				ArrayList<OnceBean> once = new ArrayList<OnceBean>();
-				while (rs.next()) {
-					OnceBean bean = new OnceBean();
-					bean.setOnceId(rs.getInt("once_id"));
-					bean.setText(rs.getString("text"));
-					bean.setReserveTime(rs.getString("reserve_time"));
-					bean.setPosted(rs.getInt("posted"));
-					once.add(bean);
-				}
-				rs.close();
-				OnceBeans.add(once);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			sql = String.format(once_format, year, month + 2, nextMonthDay,
+					year, month + 2, nextMonthDay);
+			rs = setOnceBeans(dbm, sql, rs);
 
-			int week = (count + 1) % 7;
-			if (week == 0) {
-				week = 7;
-			}
-			presql1 = String.format(
-					"SELECT routine_id FROM  weekly WHERE day=%d", week);
-			presql2 = String.format(
-					"SELECT routine_id FROM  monthly WHERE day=%d",
-					nextMonthDay);
+			int week = setWeek(count);
+			presql1 = String.format(weekly_format, week);
+			presql2 = String.format(monthly_format, nextMonthDay);
 			ArrayList<Integer> ids = new ArrayList<Integer>();
 			try {
 				ArrayList<RoutineBean> routine = new ArrayList<RoutineBean>();
-				rs = dbm.getResultSet(presql1);
-				while (rs.next()) {
-					ids.add(rs.getInt("routine_id"));
-				}
-				rs.close();
-				rs = dbm.getResultSet(presql2);
-				while (rs.next()) {
-					ids.add(rs.getInt("routine_id"));
-				}
-				rs.close();
+				rs = addRoutineCand(dbm, presql1, ids);
+				rs = addRoutineCand(dbm, presql2, ids);
 				for (int j = 0; j < ids.size(); j++) {
-					sql = String
-							.format(
-									"SELECT * FROM routine WHERE start_date<='%04d-%02d-%02d' and end_date>='%04d-%02d-%02d' and routine_id=%d  and posted=0",
-									year, month + 2, nextMonthDay, year,
-									month + 2, nextMonthDay, ids.get(j));
-					rs = dbm.getResultSet(sql);
+					sql = String.format(routine_format, year, month + 2,
+							nextMonthDay, year, month + 2, nextMonthDay, ids
+									.get(j));
 
-					while (rs.next()) {
-						RoutineBean bean = new RoutineBean();
-						bean.setRoutineId(rs.getInt("routine_id"));
-						bean.setTitle(rs.getString("title"));
-						bean.setText(rs.getString("text"));
-						bean.setStartDate(rs.getString("start_date"));
-						bean.setEndDate(rs.getString("end_date"));
-						bean.setPosted(rs.getInt("posted"));
-						routine.add(bean);
-					}
-					rs.close();
+					rs = addRoutine(dbm, sql, routine);
 
 				}
 				RoutineBeans.add(routine);
@@ -323,10 +185,96 @@ public class Scheduler {
 		session.setAttribute("calendarDay", calendarDay);
 		session.setAttribute("OnceBeans", OnceBeans);
 		session.setAttribute("RoutineBeans", RoutineBeans);
+		show();
+	}
+
+	/**
+	 * @param count
+	 * @return
+	 */
+	private int setWeek(int count) {
+		int week = (count + 1) % 7;
+		if (week == 0) {
+			week = 7;
+		}
+		return week;
+	}
+
+	/**
+	 * @param dbm
+	 * @param presql1
+	 * @param ids
+	 * @return
+	 * @throws SQLException
+	 */
+	private ResultSet addRoutineCand(DBManager dbm, String presql1,
+			ArrayList<Integer> ids) throws SQLException {
+		ResultSet rs;
+		rs = dbm.getResultSet(presql1);
+		while (rs.next()) {
+			ids.add(rs.getInt("routine_id"));
+		}
+		rs.close();
+		return rs;
+	}
+
+	/**
+	 * @param dbm
+	 * @param sql
+	 * @param routine
+	 * @return
+	 * @throws SQLException
+	 */
+	private ResultSet addRoutine(DBManager dbm, String sql,
+			ArrayList<RoutineBean> routine) throws SQLException {
+		ResultSet rs;
+		rs = dbm.getResultSet(sql);
+		while (rs.next()) {
+			RoutineBean bean = new RoutineBean();
+			bean.setRoutineId(rs.getInt("routine_id"));
+			bean.setTitle(rs.getString("title"));
+			bean.setText(rs.getString("text"));
+			bean.setStartDate(rs.getString("start_date"));
+			bean.setEndDate(rs.getString("end_date"));
+			bean.setPosted(rs.getInt("posted"));
+			routine.add(bean);
+		}
+		rs.close();
+		return rs;
+	}
+
+	/**
+	 * @param dbm
+	 * @param sql
+	 * @param rs
+	 * @return
+	 */
+	private ResultSet setOnceBeans(DBManager dbm, String sql, ResultSet rs) {
+		try {
+			rs = dbm.getResultSet(sql);
+			ArrayList<OnceBean> once = new ArrayList<OnceBean>();
+			while (rs.next()) {
+				OnceBean bean = new OnceBean();
+				bean.setOnceId(rs.getInt("once_id"));
+				bean.setText(rs.getString("text"));
+				bean.setReserveTime(rs.getString("reserve_time"));
+				bean.setPosted(rs.getInt("posted"));
+				once.add(bean);
+			}
+			rs.close();
+			OnceBeans.add(once);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	/**
+	 * コンソール上での確認を行う
+	 */
+	private void show() {
 		for (int i = 0; i < OnceBeans.size(); i++) {
-			int week = (i + 1) % 7;
-			if (week == 0)
-				week = 7;
+			int week = setWeek(i);
 			System.out.println(String.format("%02d日 %s曜日", calendarDay[i],
 					int2week(week)));
 			System.out.println("単発ツイート　" + OnceBeans.get(i).size() + "件");
@@ -404,22 +352,23 @@ public class Scheduler {
 			return "none";
 		}
 	}
-	
+
 	/**
 	 * yearとmonthをセットする
+	 * 
 	 * @param strYear
 	 * @param strMonth
-	 * @param session 
+	 * @param session
 	 */
-	public void setYM(String strYear,String strMonth,HttpSession session){
-		if (session.getAttribute("sche")!=null){
+	public void setYM(String strYear, String strMonth, HttpSession session) {
+		if (session.getAttribute("sche") != null) {
 			int newYear = new Integer(strYear).intValue();
 			int newMonth = new Integer(strMonth).intValue();
 			year = newYear;
-			month= newMonth-1;
-		}else{
-			year=0;
-			month=0;
+			month = newMonth - 1;
+		} else {
+			year = 0;
+			month = 0;
 		}
 	}
 }
